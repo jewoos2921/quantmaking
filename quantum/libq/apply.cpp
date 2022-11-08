46fa7e4090f82cd1dd5c77e3db9d55081edd2819
jewoo <jewoos@gmail.com>
2022-11-08 14:12:52 +0900

    quantum 6~8 libq

--- quantum/libq/apply.cpp
+++ quantum/libq/apply.cpp
@@ -54,6 +54,126 @@ namespace libq {
     }
 
     void libq_reconstruct_hash(qureg *reg) {
+        reg->hash_computes += 1;
+        if (reg->hash_cashing && reg->hits < HASH_CASH_SIZE) {
+            for (int i = 0; i < reg->hits; ++i) {
+                reg->hash[reg->hash_hits[i]] = 0;
+                reg->hash_hits[i] = 0;
+            }
+            reg->hits = 0;
+        } else {
+            memset(reg->hash, 0, (1 << reg->hash_w) * sizeof(int));
+            memset(reg->hash_hits, 0, reg->hits * sizeof(int));
+            reg->hits = 0;
+            // TODO(rhundt): Apparently the compiler doesn't convert this loop,
+            // Investigate.
+//            for (int i = 0; i < (i << reg->hash_w); ++i) {
+//                reg->hash[i] = 0;
+//            }
+        }
+        for (int i = 0; i < reg->size; ++i) {
+            libq_add_hash(reg->state[i], i, reg);
+        }
+    }
 
+    void libq_gate1(int target, cmplx m[4], qureg *reg) {
+        int add_size{0};
+
+        libq_reconstruct_hash(reg);
+
+        // calculae the number of basis states to be added.
+        for (int i = 0; i < reg->size; ++i) {
+            /* determine whether XORed basis state already exists. */
+            if (get_state(reg->state[i] ^ (static_cast<state_t>(1) << target), reg) == static_cast<state_t>(-1)) {
+                add_size++;
+            }
+
+            /* allocate memory for the new basis states */
+            if (add_size) {
+                reg->state = static_cast<state_t *>(
+                        realloc(reg->state, (reg->size + add_size) * sizeof(state_t)));
+                reg->amplitude = static_cast<cmplx *>(
+                        realloc(reg->amplitude, (reg->size + add_size) * sizeof(cmplx)));
+
+                memset(&reg->state[reg->size], 0, add_size * sizeof(int));
+                memset(&reg->amplitude[reg->size], 0, add_size * sizeof(cmplx));
+
+                if (reg->size + add_size > reg->max_size) { reg->max_size = reg->size + add_size; }
+            }
+        }
+        char *done = static_cast<char *>(calloc(reg->size + add_size,
+                                                sizeof(char)));
+        int next_state = reg->size;
+        float limit = (1.0 / (static_cast<state_t>(1) << reg->width)) * 1e-6;
+
+        /* perform the actual matix multiplication */
+        for (int i = 0; i < reg->size; ++i) {
+            if (!done[i]) {
+                /* determine if the target of the basis state is set */
+                int is_set = reg->state[i] & (static_cast<state_t>(1) << target);
+                int xor_index =
+                        get_state(reg->state[i] ^ (static_cast<state_t>(1) << target), reg);
+                cmplx tnot = xor_index >= 0 ? reg->amplitude[xor_index] : 0;
+                cmplx t = reg->amplitude[i];
+
+                if (is_set) {
+                    reg->amplitude[i] = m[2] * tnot + m[3] * t;
+                } else {
+                    reg->amplitude[i] = m[0] * t + m[1] * tnot;
+                }
+
+                if (xor_index >= 0) {
+                    if (is_set) {
+                        reg->amplitude[xor_index] = m[0] * tnot + m[1] * t;
+                    } else {
+                        reg->amplitude[xor_index] = m[2] * t + m[3] * tnot;
+                    }
+                } else { /* new basis state will be created */
+                    if (std::abs(m[1]) == 0.0 && is_set) { break; }
+                    if (std::abs(m[2]) == 0.0 && !is_set) { break; }
+
+                    reg->state[next_state] =
+                            reg->state[i] ^ (static_cast<state_t>(1) << target);
+                    reg->amplitude[next_state] = is_set ? m[1] * t : m[2] * t;
+                    next_state += 1;
+                }
+                if (xor_index >= 0) {
+                    done[xor_index] = 1;
+                }
+            }
+        }
+
+        reg->size += add_size;
+        free(done);
+
+        /* remove basis states with extremply small amplitude. */
+        if (reg->hash_w) {
+            int dec_size{0};
+            for (int i = 0, j = 0; i < reg->size; ++i) {
+                if (probability(reg->amplitude[i]) < limit) {
+                    j++;
+                    dec_size++;
+                } else if (j) {
+                    reg->state[i - j] = reg->state[i];
+                    reg->amplitude[i - j] = reg->amplitude[i];
+                }
+            }
+
+            if (dec_size) {
+                reg->size -= dec_size;
+
+                // These seem redundant.
+//                reg->amplitude = static_cast<cmplx *>(
+//                        realloc(reg->amplitude, reg->size * sizeof(cmplx)));
+//                reg->state = static_cast<state_t *>(
+//                        realloc(reg->state, reg->size * sizeof(state_t)));
+            }
+        }
+
+        if (reg->size > (1 << (reg->hash_w - 1))) {
+            fprintf(stderr,
+                    "Warning: inefficient hash table (size %i vs hash %i)\n",
+                    reg->size, 1 << reg->hash_w);
+        }
     }
 }
--- quantum/libq/gates.cpp
+++ quantum/libq/gates.cpp
@@ -1,3 +1,86 @@
 //
 // Created by jewoo on 2022-11-06.
 //
+#include <cstdio>
+#include <cmath>
+
+#include "libq.h"
+
+namespace libq {
+
+    void v(int target, qureg *reg) {
+        static cmplx mv[4] = {
+                cmplx(0.5, 0.5), cmplx(0.5, 0.5),
+                cmplx(0.5, -0.5), cmplx(0.5, 0.5)
+        };
+        for (int i = 0; i < reg->size; ++i) {
+            libq_gate1(target, mv, reg);
+        }
+    }
+
+    void x(int target, qureg *reg) {
+        for (int i = 0; i < reg->size; ++i) {
+            reg->bit_xor(i, target);
+        }
+    }
+
+    void y(int target, qureg *reg) {
+        for (int i = 0; i < reg->size; ++i) {
+            reg->bit_xor(i, target);
+            if (reg->bit_is_set(i, target)) {
+                reg->amplitude[i] *= cmplx(0, 1.0);
+            } else {
+                reg->amplitude[i] *= cmplx(0, -1.0);
+            }
+        }
+    }
+
+    void z(int target, qureg *reg) {
+        for (int i = 0; i < reg->size; ++i) {
+            if (reg->bit_is_set(i, target)) {
+                reg->amplitude[i] *= -1;
+            }
+        }
+    }
+
+    void h(int target, qureg *reg) {
+        static cmplx mh[4] = {
+                sqrt(1.0 / 2), sqrt(1.0 / 2),
+                sqrt(1.0 / 2), -sqrt(1.0 / 2)
+        };
+
+        libq_gate1(target, mh, reg);
+    }
+
+    void yroot(int target, qureg *reg) {
+        static cmplx mv[4] = {
+                cmplx(0.5, 0.5), cmplx(-0.5, -0.5),
+                cmplx(0.5, 0.5), cmplx(0.5, 0.5)
+        };
+        for (int i = 0; i < reg->size; ++i) {
+            libq_gate1(target, mv, reg);
+        }
+    }
+
+    void walsh(int width, qureg *reg) {
+        for (int i = 0; i < width; ++i) {
+            h(i, reg);
+        }
+    }
+
+    cmplx cexp(float phi) {
+        return cmplx(std::cos(phi), 0.0) +
+               cmplx(0.0, 1.0) * cmplx(std::sin(phi), 0.0);
+    }
+
+    void u1(int target, float gamma, qureg *reg) {
+        cmplx z = cexp(gamma);
+        for (int i = 0; i < reg->size; ++i) {
+            if (reg->bit_is_set(i, target)) {
+                reg->amplitude[i] *= z;
+            }
+        }
+    }
+
+
+}
